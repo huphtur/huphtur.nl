259f90c6ac21c286fb3fb94e2425835c99b54195
M. Appelman <huphtur@icloud.com>
2020-08-24 10:31:00 -0700

    canonical setup 📷

--- src/_data/site.js
+++ src/_data/site.js
@@ -3,7 +3,7 @@ module.exports = {
   title: 'Marcel Appelman',
   description:
     'Tall, skinny Dutch guy with a penchant for cycling, skateboarding, and nerdy computer crap.',
-  url: 'https://huphtur.nl', // don't end with a slash /
+  url: 'https://huphtur.netlify.app', // don't end with a slash /
   author: {
     name: 'Marcel Appelman',
     email: 'huphtur@gmail.com',
