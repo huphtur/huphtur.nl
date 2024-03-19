dd073679ade64832f7c1764b30020f16ff0a3022
Marcel Appelman <huphtur@icloud.com>
2024-03-18 19:02:46 -0700

    utilize 11ty image plugin 🌠

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,12 +1,12 @@
 import embedEverything from 'eleventy-plugin-embed-everything';
-import img2picture from 'eleventy-plugin-img2picture';
 import pluginRss from '@11ty/eleventy-plugin-rss';
 import markdownIt from "markdown-it";
+import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 
 const INPUT_DIR = './src';
 const OUTPUT_DIR = './_site/images';
 
-export default async function(eleventyConfig) {
+export default async function (eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy('src/styles.css');
   eleventyConfig.addPassthroughCopy('src/favicon.ico');
@@ -16,6 +16,7 @@ export default async function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/icon-512.png');
   eleventyConfig.addPassthroughCopy('src/site.webmanifest');
   eleventyConfig.addPassthroughCopy('src/robots.txt');
+  eleventyConfig.addPassthroughCopy('src/images');
 
   eleventyConfig.addPlugin(pluginRss);
 
@@ -27,26 +28,27 @@ export default async function(eleventyConfig) {
 
   eleventyConfig.setLibrary("md", markdownIt(options));
 
-  if (process.env.ELEVENTY_ENV === 'production') {
-    // also copy images for meta stuff
-    eleventyConfig.addPassthroughCopy('src/images');
-    eleventyConfig.addPlugin(img2picture, {
-      eleventyInputDir: INPUT_DIR,
-      imagesOutputDir: OUTPUT_DIR,
-      urlPath: '/images/',
-      formats: ['webp'],
-      // sizes: '(min-width: 960px) 720px, 100vw',
-      minWidth: 480,
-      maxWidth: 2400,
-      widthStep: 320,
-      // sharpAvifOptions: {
-      //   lossless: true,
-      //   quality: 1,
-      // },
-    });
-  } else {
-    eleventyConfig.addPassthroughCopy('src/images');
-  }
+  eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
+    // which file extensions to process
+    extensions: "html",
+
+    // Add any other Image utility options here:
+
+    // optional, output image formats
+    formats: ["webp"],
+    // formats: ["auto"],
+
+    // optional, output image widths
+    // widths: ["auto"],
+    widths: [600, 1200, "auto"],
+
+    // optional, attributes assigned on <img> override these values.
+    defaultAttributes: {
+      sizes: "auto",
+      loading: "lazy",
+      decoding: "async"
+    }
+  });
 
   eleventyConfig.addPlugin(embedEverything, {
     use: ['youtube', 'soundcloud'],
