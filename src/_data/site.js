67907c9a7bf37eb2c4c99776793cea324614bc34
Marcel Appelman <huphtur@icloud.com>
2024-09-29 15:53:38 -0700

    New Sidebar 🍫
    * take 1
    
    * small viewports done
    
    * move to side
    
    * all aligned now
    
    * notchgate
    
    * silly hack to avoid more markup
    
    * new width
    
    * now with better flex
    
    * make main area focusable
    
    * sum padding
    
    * Merge branch 'master' into better-sidebar

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,7 +1,7 @@
 export default {
   title: 'huphtur',
   description:
-    'Tall, Dutch guy with a penchant for cycling, skateboarding, music, and nerdy computer stuff.',
+    'Tall Dutch guy with a penchant for cycling, skateboarding, music, and nerdy computer stuff.',
   url: 'https://huphtur.nl',
   author: {
     name: 'Marcel Appelman',
