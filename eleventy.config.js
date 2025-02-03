879fef6731985dd945c6f0cb8867555ce1f8fcad
Marcel Appelman <huphtur@icloud.com>
2025-02-02 19:47:14 -0800

    add image optis 🎆

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,6 +2,7 @@ import markdownIt from 'markdown-it';
 import pluginRss from '@11ty/eleventy-plugin-rss';
 import embeds from 'eleventy-plugin-embed-everything';
 import eleventyNavigationPlugin from "@11ty/eleventy-navigation";
+import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 
 export default async function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/bundle.css");
@@ -21,6 +22,23 @@ export default async function (eleventyConfig) {
 
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
   eleventyConfig.addPlugin(pluginRss);
+
+	eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
+		// output image formats
+		formats: ["avif", "webp", "jpeg"],
+
+		// output image widths
+		widths: ["auto"],
+
+		// optional, attributes assigned on <img> nodes override these values
+		htmlOptions: {
+			imgAttributes: {
+				loading: "lazy",
+				decoding: "async",
+			},
+			pictureAttributes: {}
+		},
+	});
 
   eleventyConfig.addPlugin(embeds, {
     use: ['youtube', 'soundcloud'],
